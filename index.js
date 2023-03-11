7a74a36d2d44d9c0be49e5c3ba15948c6dcc14b6
parwez <parwezzafar3@gmail.com>
2023-03-12 00:28:57 +0530

    adding my order router and functionality

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.get('/', (req, res) => {
 app.use(express.json())
 app.use('/api', require('./Routes/CreateUser'));
 app.use('/api', require('./Routes/DisplayFoodItem'));
+app.use('/api', require('./Routes/orderData'));
 
 app.listen(port, () => {
     console.log("server started at port " + port);
