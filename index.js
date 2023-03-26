753ce852ca61ea3f09957c13fa8baaac93eb9df2
parwez <parwezzafar3@gmail.com>
2023-03-26 19:27:37 +0530

    adding start command

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 const dotenv = require('dotenv');
 dotenv.config({
     path: './config.env',
