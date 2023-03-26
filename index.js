1b273d8da125e5edbb5bfe174a351cdca0fda496
parwez <parwezzafar3@gmail.com>
2023-03-26 22:21:15 +0530

    bug fix

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8000;
+const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config({
     path: './config.env',
 })
 
+
+app.use(cors({
+    origin: "https://jocular-kangaroo-fa1f11.netlify.app"
+}))
+
 const dbconnection = require('./db');
 dbconnection();
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Reqested-With, Content-Type, Accept",
-    );
-    next();
-})
+
+
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+//     res.header(
+//         "Access-Control-Allow-Headers",
+//         "Origin, X-Reqested-With, Content-Type, Accept",
+//     );
+//     next();
+// })
 
 app.get('/', (req, res) => {
     res.send("this is on '/' router");
